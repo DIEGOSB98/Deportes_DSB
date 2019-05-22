819b64b6c1685f7393363787c6210f2387809dbe
DiegoBinario <diegosbquimica@gmail.com>
2019-05-22 18:37:28 -0400

    Cambios acerca-de

--- app_epe2/src/app/acerca-de-pagina/acerca-de-pagina.page.ts
+++ app_epe2/src/app/acerca-de-pagina/acerca-de-pagina.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActionSheetController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-acerca-de-pagina',
   templateUrl: './acerca-de-pagina.page.html',
@@ -7,7 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcercaDePaginaPage implements OnInit {
 
-  constructor() { }
+  constructor(public AsheeCtrl: ActionSheetController, public ToastCtrl: ToastController) { }
+
+  async mostrar_calificacion_buena() {
+    const mensaje = await this.ToastCtrl.create(
+      {
+        message: "GRACIAS POR SU APOYO!!!",
+        duration: 2000,
+
+      }
+    );
+    mensaje.present();
+  }
+
+  async mostrar_calificacion_mala() {
+    const mensaje = await this.ToastCtrl.create(
+      {
+        message: "SEGUIREMOS TRANAJANDO PARA MEJORAR :c",
+        duration: 2000,
+
+      }
+    );
+    mensaje.present();
+  }
+
+  async Mostrat_op() {
+    const hojaDeAccion = await this.AsheeCtrl.create({
+
+      header: 'opciones',
+      buttons:
+        [
+          {
+            text: 'Muy buena',
+            icon: 'happy',
+            handler: () => {
+              this.mostrar_calificacion_buena();
+            }
+
+          }, {
+            text: 'Falta Mejorar',
+            icon: 'sad',
+            handler: () => {
+              this.mostrar_calificacion_mala();
+            }
+          }
+        ]
+    });
+    hojaDeAccion.present();
+
+  }
 
   ngOnInit() {
   }
